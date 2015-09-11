997307f379f1e8267bfc438b15bc82080dfa0ce2
Liang Zeng <michaeltsengcs@gmail.com>
2015-09-11 14:29:09 -0500

    Use enumeration instead of magic number

--- LeetCode/Liang/75SortColors.cpp
+++ LeetCode/Liang/75SortColors.cpp
@@ -2,22 +2,24 @@
  * 1. Counting sort
  * 2. Two pointer
  **/
- 
+
+enum Color : int { RED = 0, WHITE = 1, BLUE = 2 };
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int zeroIdx = 0, test = 0, twoIdx = (int)nums.size() - 1;
         while (test <= twoIdx) {
-            if (nums.at(test) == 0) {
+            if (nums.at(test) == RED) {
                 swap(nums.at(test), nums.at(zeroIdx));
                 ++test;
                 ++zeroIdx;
-            } else if (nums.at(test) == 1) {
+            } else if (nums.at(test) == WHITE) {
                 ++test;
-            } else if (nums.at(test) == 2) {
+            } else if (nums.at(test) == BLUE) {
                 swap(nums.at(test), nums.at(twoIdx));
                 --twoIdx;
             }
         }
     }
-};
+}; 
