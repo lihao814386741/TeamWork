dbb48e6f6eca5e18648fb6a738f1a1ef88ba2d1e
Liang Zeng <michaeltsengcs@gmail.com>
2015-09-10 23:11:07 -0500

    1. Add suffix 2. LeetCode 35: search insert postion

---
+++ LeetCode/Liang/35SearchInsertPosition.cpp
@@ -0,0 +1,20 @@
+ass Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int begin = 0, end = nums.size() - 1;
+        while (begin < end) {
+            int mid = begin + (end - begin) / 2;
+            if (target <= nums.at(mid)) {
+                end = mid;
+            } else {
+                begin = mid + 1;
+            }
+        }
+        
+        if (target > nums.at(begin)) {
+            ++begin;
+        }
+        
+        return begin;
+    }
+};
